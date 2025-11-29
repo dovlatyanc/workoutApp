4b4b0b7283c88c9754b13bc89f42e4988fb0abc5
dovlatyanc <dovlatyanc.ru@gmail.com>
2025-11-29 17:24:55 +0300

    добавлен минимальный хэдер

--- wa-client/src/App.jsx
+++ wa-client/src/App.jsx
@@ -7,18 +7,8 @@ function App() {
 	return (
 		<Layout>
 			<div className='App'>
-				<h1>Vite + React</h1>
-				<div className='card'>
-					<button onClick={() => setCount(count => count + 1)}>
-						count is {count}
-					</button>
-					<p>
-						Edit <code>src/App.jsx</code> and save to test HMR
-					</p>
-				</div>
-				<p className='read-the-docs'>
-					Click on the Vite and React logos to learn more
-				</p>
+				<h1>Лучшее приложение для тренировок епта</h1>
+				
 			</div>
 		</Layout>
 	)
--- wa-client/src/components/layout/header/Header.jsx
+++ wa-client/src/components/layout/header/Header.jsx
@@ -1,7 +1,22 @@
+import { FiArrowLeft } from 'react-icons/fi'
+import { useAuth } from '../../../hooks/useAuth'
+import Hamburger from '../hamburger/Hamburger'
 import styles from './Header.module.scss'
 
-const Header = () => {
-	return <div className={styles.header}>Header</div>
+const Header = ({ backLink }) => {
+	/* TODO: React router useHistory */
+
+	const { isAuth } = useAuth()
+
+	return (
+		<header className={styles.header}>
+			<button onClick={() => {}}>
+				<FiArrowLeft color="#fff" size={24} />
+			</button>
+			{/* User profile */}
+			<Hamburger  />
+		</header>
+	)
 }
 
 export default Header
